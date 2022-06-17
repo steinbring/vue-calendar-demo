cd31705918269a99aad7999c6f407f5e545d860a
Joe Steinbring <joe@jws.app>
2022-06-17 14:26:07 -0500

    Initial Commit of FullCalendar vue3 demo
    Previously, I created https://codepen.io/steinbring/pen/qBxJrQj as a demo of how to use FullCalendar 5.x with a pure javascript app.  This is for a demo of how to use it with Vue3 and Vue CLI.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <TheCalendar />
+</template>
+
+<script>
+import TheCalendar from './components/TheCalendar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheCalendar
+  }
+}
+</script>
---
+++ src/components/TheCalendar.vue
@@ -0,0 +1,34 @@
+<script>
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        // Don't show the weekends
+        weekends: false,
+        // Define the header for the calendar
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        // Define Event Data
+        events: 'https://gist.githubusercontent.com/steinbring/80157cc5866de53c8cd975a1673f4425/raw/72d8dfb2880b76ffbd7bb8a48f3803aab3b804ba/events.json'
+      }
+    }
+  }
+}
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
